a0e8c9112340087b3f5f077ae5709d175f6a51f8
Xalamander01 <aleksandarstanchev@abv.bg>
2022-10-27 17:46:50 +0300

    app crashes when moving to third activity, trying to find out why

--- app/src/main/java/com/example/workoutshuffleandroid/Selector.java
+++ app/src/main/java/com/example/workoutshuffleandroid/Selector.java
@@ -1,11 +1,13 @@
 package com.example.workoutshuffleandroid;
 
-import android.media.Image;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class Selector extends AppCompatActivity {
 
@@ -16,6 +18,25 @@ public class Selector extends AppCompatActivity {
             setContentView(R.layout.selector);
 
             ImageView backButton = findViewById(R.id.backButton);
+            ImageView lifterButton = findViewById(R.id.lifterImageSelector);
+            Button fullBodyButton = findViewById(R.id.fullBodyButton);
+
+            ArrayList<Button> buttonArrayList = new ArrayList<>();
+            buttonArrayList.add(findViewById(R.id.fullBodyButton));
+            buttonArrayList.add(findViewById(R.id.upperButton));
+            buttonArrayList.add(findViewById(R.id.lowerButton));
+            buttonArrayList.add(findViewById(R.id.pushButton));
+            buttonArrayList.add(findViewById(R.id.pullButton));
+            buttonArrayList.add(findViewById(R.id.legsButton));
+
+            fullBodyButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        goToWorkoutPrinter(null, null);
+                        //System.out.println(String.valueOf(fullBodyButton.getId()));
+                    }
+            });
+
             backButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -23,11 +44,10 @@ public class Selector extends AppCompatActivity {
                 }
             });
         }
-        /*
-    private void retrieveMessage() {
-        result = findViewById(R.id.activity_second_result);
-        result.setText(getIntent().getStringExtra("message"));
-    }
 
-         */
+    private void goToWorkoutPrinter(String key, String value) {
+        Intent goToWorkoutPrinter = new Intent(this, WorkoutPrinter.class);
+        goToWorkoutPrinter.putExtra(key,value);
+        startActivity(goToWorkoutPrinter);
+    }
 }
--- app/src/main/java/com/example/workoutshuffleandroid/WorkoutPrinter.java
+++ app/src/main/java/com/example/workoutshuffleandroid/WorkoutPrinter.java
@@ -1,6 +1,9 @@
 package com.example.workoutshuffleandroid;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WorkoutPrinter extends AppCompatActivity {
@@ -10,6 +13,10 @@ public class WorkoutPrinter extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         overridePendingTransition(R.anim.fadein, R.anim.fadeout);
         setContentView(R.layout.workout_printer);
+
+        //String workoutType = getIntent().getStringExtra("workoutType");
+        //TextView firstExerciseText = findViewById(R.id.firstExercise);
+        //firstExerciseText.setText(workoutType);
 /*
         message = findViewById(R.id.clickCounter);
         ImageView bro = findViewById(R.id.lifterImage);
@@ -23,5 +30,12 @@ public class WorkoutPrinter extends AppCompatActivity {
         });
 
  */
+        ImageView lifterImagePrinter = findViewById(R.id.lifterImagePrinter);
+        lifterImagePrinter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
